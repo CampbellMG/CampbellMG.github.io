c24da7fd32fda0f0dd719db4d1005c665c0ca463
CampbellMG <campbell.gorman@gmail.com>
2020-01-14 13:15:44 +1100

    Rebuilt [14/01/2020-13:15:44]

--- js/regexoff-redirect.js
+++ js/regexoff-redirect.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var params = decodeURIComponent(document.location.search);
     if (params && params.indexOf("pattern=") !== -1) {
-        window.location.assign("regexoff://" + params)
+        window.location.assign("regexoff://pattern?id=" + params.split("pattern=")[1])
     }
 });
